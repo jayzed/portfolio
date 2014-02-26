4d3951f916c4f99bfad2c3a7af15d43d10a28751
Jordy Zagdanski <jayzed@gmail.com>
2014-02-26 00:34:01 -0500

    Add portfolio project page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,10 +8,15 @@ class PagesController < ApplicationController
   def about
   end
 
+  # Project Pages
+
   def frontier
   end
 
   def secondmarket
+  end
+
+  def portfolio
   end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,10 @@ Portfolio::Application.routes.draw do
   get "about" => "pages#about"
   get "resume" => "pages#resume"
 
+  # Project Pages
   get "frontier" => "pages#frontier"
   get "secondmarket" => "pages#secondmarket"
+  get "portfolio" => "pages#portfolio"
 
 
   # Example of regular route:
